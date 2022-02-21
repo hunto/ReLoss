c1afcb6c83667b75855332774704e8b72773145e
hunto <hunto@foxmail.com>
2022-02-21 17:03:17 +0800

    clean code

--- reloss/nat.py
+++ reloss/nat.py
@@ -14,7 +14,13 @@ from torch.hub import load_state_dict_from_url
 
 
 class ReLoss(nn.Module):
-    def __init__(self, args, vocab_size, padding_idx, fn=nn.ELU, pretrained=True):
+
+    def __init__(self,
+                 args,
+                 vocab_size,
+                 padding_idx,
+                 fn=nn.ELU,
+                 pretrained=True):
         super(ReLoss, self).__init__()
         self.logits_layer = nn.Sequential(
             nn.Linear(1, args.decoder_embed_dim),
@@ -26,9 +32,13 @@ class ReLoss(nn.Module):
         new_args.dropout = 0.1
         new_args.activation_fn = 'elu'
         self.ce = nn.CrossEntropyLoss(ignore_index=padding_idx)
-        self.pe = PositionalEmbedding(args.max_target_positions, args.decoder_embed_dim, padding_idx, learned=True)
+        self.pe = PositionalEmbedding(args.max_target_positions,
+                                      args.decoder_embed_dim,
+                                      padding_idx,
+                                      learned=True)
         self.emb_drop = nn.Dropout(new_args.dropout)
-        self.block = nn.ModuleList([TransformerEncoderLayer(new_args) for _ in range(2)])
+        self.block = nn.ModuleList(
+            [TransformerEncoderLayer(new_args) for _ in range(2)])
         self.loss_layer = nn.Sequential(
             nn.Linear(args.decoder_embed_dim, args.decoder_embed_dim),
             nn.ELU(),
@@ -40,16 +50,16 @@ class ReLoss(nn.Module):
             state_dict = load_state_dict_from_url(ckpt_url, map_location='cpu')
             self.load_state_dict(state_dict)
 
-
     def forward(self, model, logits, tgt, tgt_non_pad_mask):
         batch_size, seq_len, vocab_size = logits.size()
         pos_logits = torch.gather(logits, 2, tgt.unsqueeze(-1))
         pos_logits = pos_logits * tgt_non_pad_mask.unsqueeze(-1)
         pos_hidden = self.logits_layer(pos_logits)
         pos_hidden = self.emb_drop(pos_hidden + self.pe(tgt_non_pad_mask))
-        pos_hidden = self.block[0](pos_hidden.transpose(0, 1), ~tgt_non_pad_mask).transpose(0, 1)
-        pos_hidden = self.block[1](pos_hidden.transpose(0, 1), ~tgt_non_pad_mask).transpose(0, 1)
+        pos_hidden = self.block[0](pos_hidden.transpose(0, 1),
+                                   ~tgt_non_pad_mask).transpose(0, 1)
+        pos_hidden = self.block[1](pos_hidden.transpose(0, 1),
+                                   ~tgt_non_pad_mask).transpose(0, 1)
         loss = self.loss_layer(pos_hidden).squeeze(-1) * tgt_non_pad_mask
         loss = loss.sum(1) / (tgt_non_pad_mask.sum(1) + 1e-8)
         return loss.abs().mean().unsqueeze(0)
-
